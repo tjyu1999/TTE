49f841d9997206e56a33a97da037316a57c30e44
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-05-08 22:33:48 +0800

    Add files via upload

--- draw.py
+++ draw.py
@@ -4,14 +4,17 @@ from matplotlib import pyplot as plt
 
 
 def draw():
-    types = ['router loss', 'scheduler loss', 'router num', 'scheduler num']
+    types = ['router loss', 'scheduler loss']
 
     for data in types:
         y = np.load(f'record/{data}.npy', allow_pickle=True)
         x = np.arange(len(y))
         plt.plot(x, y, 'r')
+        plt.xlabel('Episode')
+        plt.ylabel('Loss')
         plt.grid(True)
         plt.savefig(f'plot/{data}.jpg')
+        plt.show()
 
 
 if __name__ == '__main__':
